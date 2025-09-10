94222e2827159a32c3e32c1f7a7cf9cd5e916771
Zandra7 <sandra.eng@live.no>
2025-09-10 12:12:11 +0200

    Legger til et prosjekt og gjør prosjektene klikkbare

--- src/data/prosjekter.ts
+++ src/data/prosjekter.ts
@@ -2,22 +2,35 @@ export interface Prosjekt {
 	navn: string;
 	beskrivelse: string;
 	teknologier: string[];
+	link: string;
 }
 
 export const prosjekter: Prosjekt[] = [
 	{
 		navn: 'Prosjekt 1',
 		beskrivelse: 'Beskrivelse av prosjekt 1.',
-		teknologier: ['React', 'Node.js', 'MongoDB'],
+		teknologier: ['JavaScript', 'Express', 'PostgreSQL'],
+		link: '/',
 	},
 	{
-		navn: 'Prosjekt 2',
-		beskrivelse: 'Beskrivelse av prosjekt 2.',
-		teknologier: ['Vue.js', 'Express', 'PostgreSQL'],
+		navn: 'Brainstorming-app',
+		beskrivelse:
+			'En kreativ samarbeidsplattform som lar deg og andre lage eller inngå rom der idéer deles anonymt som post-it-lapper.',
+		teknologier: [
+			'Node.js',
+			'Express.js',
+			'SQLite',
+			'HTML',
+			'CSS',
+			'JavaScript',
+		],
+		link: 'https://github.com/Zandra7/Brainstorming-app',
 	},
+
 	{
 		navn: 'Prosjekt 3',
 		beskrivelse: 'Beskrivelse av prosjekt 3.',
 		teknologier: ['Astro', 'TypeScript', 'CSS'],
+		link: '/',
 	},
 ];
